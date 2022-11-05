5eb95320714c2e4c4b5195b83969cdf44780ec15
jirka88 <laval@centrum.cz>
2022-11-05 11:20:45 +0100

    rozpracovane pridani kapitoly s validacemi

--- vyukovy-pavouk/vyukovy-pavouk/Data/Kapitola.cs
+++ vyukovy-pavouk/vyukovy-pavouk/Data/Kapitola.cs
@@ -1,8 +1,10 @@
 ﻿
+using FluentValidation;
+
 namespace vyukovy_pavouk.Data
 {
     public class Kapitola
-{
+    {
         public int Id { get; set; }
         public string Název { get; set; }
         public string Perex { get; set; }
@@ -13,5 +15,16 @@ namespace vyukovy_pavouk.Data
         public List<Videa> Videa { get; set; }
         public List<KapitolaPrerekvizita> KapitolaPrerekvizita { get; set; }
 
+    }
+    public class KapitolaValidator : AbstractValidator<Kapitola>
+    {
+        public KapitolaValidator()
+        {
+            RuleFor(n => n.Název).NotEmpty().WithMessage("Kapitola musí mít název!");
+            RuleFor(n => n.Název).MaximumLength(30).WithMessage("Název kapitoly je příliš dlouhý!");
+            RuleFor(p => p.Perex).MaximumLength(255).WithMessage("Perex je příliš dlouhý!");
+            RuleFor(c => c.Kontent).NotEmpty().WithMessage("Kapitola musí mít obsah!");
+            RuleFor(c => c.Kontent).MaximumLength(65535).WithMessage("Obsah dosáhl maximální délky!");
+        }
     }
 }
