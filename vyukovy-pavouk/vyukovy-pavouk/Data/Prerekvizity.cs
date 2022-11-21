cb95148f5cb85b8f6f47534f1c61fffcd4a21427
jirka88 <laval@centrum.cz>
2022-11-21 17:54:44 +0100

    vyreseni bugu pri vytvareni uvodni kapitoly

--- vyukovy-pavouk/vyukovy-pavouk/Data/Prerekvizity.cs
+++ vyukovy-pavouk/vyukovy-pavouk/Data/Prerekvizity.cs
@@ -4,7 +4,7 @@ namespace vyukovy_pavouk.Data
 {
     public class Prerekvizity
 {
-        public int? Id { get; set; } 
+        public int Id { get; set; } 
         public int IdPrerekvizity { get; set; }
         public List<KapitolaPrerekvizita> KapitolaPrerekvizita { get; set; }
 }
