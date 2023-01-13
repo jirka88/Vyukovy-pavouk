8959908587f41af5c104f6343125ff82b11b451d
jirka88 <laval@centrum.cz>
2023-01-13 20:04:19 +0100

    dark theme 2

--- vyukovy-pavouk/vyukovy-pavouk/wwwroot/js/Javascript.js
+++ vyukovy-pavouk/vyukovy-pavouk/wwwroot/js/Javascript.js
@@ -1,3 +1,6 @@
 ﻿function SetBodyStyle() {
     document.querySelector("body").style.background = "#1f1f1f";
 }
+function SetInputDark() {
+    document.querySelector("input").style.background = "#292929";
+}
