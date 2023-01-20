4839515f721996bbb415b061ed7079f47d019994
jirka88 <laval@centrum.cz>
2023-01-20 16:56:56 +0100

    oprava bugu + pridani podpora kontrastniho rezimu

--- vyukovy-pavouk/vyukovy-pavouk/wwwroot/js/Javascript.js
+++ vyukovy-pavouk/vyukovy-pavouk/wwwroot/js/Javascript.js
@@ -1,6 +1,9 @@
 ﻿function SetBodyStyle() {
     document.querySelector("body").style.background = "#1f1f1f";
 }
+function SetBodyStyleContrast() {
+    document.querySelector("body").style.background = "#000000";
+}
 function SetInputDark() {
     document.querySelector("input").style.background = "#292929";
 }
